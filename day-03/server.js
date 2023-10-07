64d10752c200f7aa34a02ab4fd1868b38b447848
Srikanth4533 <kondakallasrikanth991@gmail.com>
2023-10-07 12:11:32 +0530

    learned about uploading files to cloudinary

--- day-03/server.js
+++ day-03/server.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const multer = require("multer");
+const cloudinary = require("cloudinary");
+
 const path = require("path");
+const fs = require("fs");
+
 require("dotenv").config();
 require("./config/db");
+
+// Cloudinary Code
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -26,11 +37,37 @@ app.get("/", (req, res) => {
   });
 });
 
+// cloudinary.config({
+//   cloud_name: 'sample',
+//   api_key: '874837483274837',
+//   api_secret: 'a676b67565c6767a6767d6767f676fe1',
+//   secure: true
+// });
+
 app.post("/fileupload", upload.single("myfile"), (req, res) => {
-  res.status(200).json({
-    status: "success",
-    message: "File Uploaded successfully",
-  });
+  console.log(req.file);
+  // Now send this file on cloudinary i.e others server
+  cloudinary.v2.uploader.upload(
+    req.file.path,
+    { folder: "labs" },
+    function (error, result) {
+      console.log(result, error);
+      // Delete the file from Local
+      fs.unlink(req.file.path, function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(`\nDeleted file: ${req.file.filename}`);
+        }
+      });
+
+      res.status(200).json({
+        status: "success",
+        message: "File Uploaded successfully",
+        data: result,
+      });
+    }
+  );
 });
 
 app.listen(process.env.PORT, () => {
